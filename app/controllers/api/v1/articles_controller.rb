4dce0ed51a030fa88333fb35bd0eebc4545af165
Gabriela Colombo <65355183+Gabicolombo@users.noreply.github.com>
2023-08-20 15:18:28 -0300

    finalizando o crud

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -3,6 +3,24 @@ class Api::V1::ArticlesController < ApplicationController
     articles = Article.all()
     render json:articles, status: 200
   end
+  
+  def new
+    article = Article.new
+  end
+
+  def create
+    article = Article.new(
+      title: article_params[:title], 
+      body: article_params[:body], 
+      author: article_params[:author]
+    )
+
+    if article.save
+      render json: article, status: 200
+    else 
+      render json: { error: 'Creating error' }
+    end
+  end
 
   def show
     article = Article.find(params[:id])
@@ -12,4 +30,33 @@ class Api::V1::ArticlesController < ApplicationController
       render json: { error: "No articles found" }
     end
   end
+
+  def update
+    article = Article.find(params[:id])
+    if article
+      article.update(
+        title: params[:title], 
+        body: params[:body], 
+        author: params[:author]
+      )
+
+      render json: "Article updated successfully"
+    else
+      render json: { error: "No articles found" }
+    end
+  end
+
+  def destroy
+    article = Article.find(params[:id])
+    if article
+      article.destroy
+      render json: "Article deleted successfully"
+    end
+  end
+
+  private
+  def article_params
+    params.require(:article).permit(:title, :body, :author)
+  end
+
 end
