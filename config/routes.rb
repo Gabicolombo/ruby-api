5094ebe3cdab29231916f3391a0c3b8c6a3cebe9
Gabriela Colombo <65355183+Gabicolombo@users.noreply.github.com>
2023-08-26 18:42:25 -0300

    utilizando o resources para o crud

--- config/routes.rb
+++ config/routes.rb
@@ -6,11 +6,12 @@ Rails.application.routes.draw do
   namespace :api do
     namespace:v1 do
       root "articles#index"
-      get "/articles", to: "articles#index"
-      get "/articles/:id", to: "articles#show"
-      post "/articles", to: "articles#create"
-      put "/articles/:id", to: "articles#update"
-      delete "/articles/:id", to: "articles#destroy"
+      # get "/articles", to: "articles#index"
+      # get "/articles/:id", to: "articles#show"
+      # post "/articles", to: "articles#create"
+      # put "/articles/:id", to: "articles#update"
+      # delete "/articles/:id", to: "articles#destroy"
+      resources :articles, only:[:index, :show, :create, :update, :destroy, :destroy]
     end
   end
 end
