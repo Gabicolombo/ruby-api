6690c79795393de4347bac9a566ea0461a7e15fe
Gabriela Colombo <65355183+Gabicolombo@users.noreply.github.com>
2023-08-26 19:18:07 -0300

    criando o primeiro item do crud (get)

--- frontend/src/app/services/article.service.ts
+++ frontend/src/app/services/article.service.ts
@@ -1,9 +1,32 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ArticleService {
+  url: string = "http://localhost:3000";
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  listArticle(){
+    return this.http.get(`${this.url}/api/v1/articles`);
+  }
+
+  addArticle(){
+
+  }
+
+  findArticle(){
+
+  }
+
+  updateArticle(){
+
+  }
+
+  deleteArticle(){
+
+  }
+
 }
